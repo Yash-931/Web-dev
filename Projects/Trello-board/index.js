141daf2c2af7c37cd72b8f59440c101fb64f195e
Yash Aggarwal <yaggarwal931@gmail.com>
2026-05-10 08:52:39 +0530

    Trello-board done - update endpoint remains

--- Projects/Trello-board/index.js
+++ Projects/Trello-board/index.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { authMiddleware } = require("./middleware");
-const { userModel, organizationModel } = require("./models");
-
-const BOARDS = []
-const ISSUES = []
-
-let BOARD_ID = 1;
-let ISSUE_ID = 1;
+const { userModel, organizationModel, boardModel, issuesModel } = require("./models");
 
 const app = express();
 app.use(express.json());
@@ -124,12 +118,97 @@ app.post("/add-member-to-organization",  authMiddleware, async (req, res) => {
     })
 })
 
-app.post("/board", (req, res) => {
+app.post("/board/:org", authMiddleware, async (req, res) => {
+    const title = req.body.title;
+    const organizationId = req.params.org;
+    const userId = req.userId;
+
+    const organization = await organizationModel.findOne({
+        _id: organizationId
+    });
+
+    if(!organization || organization.admin != userId){
+        res.status(403).json({
+            message: "Either the org doesn't exists or you are not the admin of the org"
+        })
+        return;
+    }
+
+    const board = await boardModel.create({
+        title: title,
+        organization: organizationId,
+        issues: [],
+    });
+
+    res.status(201).json({
+        message: "Board created success",
+        boardId: board._id
+    })
 
 })
 
-app.post("/issue", (req, res) => {
+app.post("/issue/:org", authMiddleware, async (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const organizationId = req.params.org;
+    const userId = req.userId;
 
+    const organization = await organizationModel.findOne({
+        _id: organizationId
+    })
+
+    const response = organization.members.filter( (memberId) => memberId.toString() === userId.toString())
+
+    if(!organization || (response.length === 0 && organization.admin.toString() != userId.toString())){
+        res.status(403).json({
+            message: "Either the org doesn't exists or you are not a member of the org"
+        })
+
+        return;
+    }
+
+    const issue = await issuesModel.create({
+        title: title,
+        description: description,
+        board: null
+    })
+
+    res.status(201).json({
+        message: "Issue created success"
+    })
+
+
+})
+
+app.post("/assign-issue/:orgId/:issueId/:boardId", async (req, res) => {
+    const issueId = req.params.issueId;
+    const boardId = req.params.boardId;
+    const userId = req.userId;
+    const organizationId = req.orgId;
+
+    const organization = await organizationModel.findOne({
+        _id: organizationId
+    })
+
+    const response = organization.members.filter( (memberId) => memberId.toString() === userId.toString())
+
+    if(!organization || (response.length === 0 && organization.admin.toString() != userId.toString())){
+        res.status(403).json({
+            message: "Either the org doesn't exists or you are not a member of the org"
+        })
+
+        return;
+    }
+
+    const issue = await issuesModel.findOne({
+        _id: issueId
+    })
+
+    const board = await boardModel.findOne({
+        _id: boardId
+    })
+
+    board.issues.push
 })
 
 
@@ -171,12 +250,71 @@ app.get("/organization", authMiddleware, async (req, res) => {
 
 })
 
-app.get("/boards", (req, res) => {
+app.get("/boards/:org", authMiddleware, async (req, res) => {
+    const organizationId = req.params.org;
+    const userId = req.userId;
 
+    const organization = await organizationModel.findOne({
+        _id: organizationId
+    })
+
+    const response = organization.members.filter( (memberId) => memberId.toString() === userId.toString())
+
+    if(!organization || (response.length === 0 && organization.admin.toString() != userId.toString())){
+        res.status(403).json({
+            message: "Either the org doesn't exists or you are not a member of the org"
+        })
+
+        return;
+    }
+
+
+    const boards = await boardModel.find({
+        organization: organizationId
+    });
+
+    res.status(200).json({
+        boards: boards
+    })
 })
 
-app.get("/issues", (req, res) => {
+app.get("/issues/:boardId", authMiddleware, async (req, res) => {
+    const boardId = req.params.boardId;
+    const userId = req.userId;
 
+    const board = await boardModel.findOne({
+        _id: boardId
+    });
+
+    const organizationId = board.organization;
+    
+    const organization = await organizationModel.findOne({
+        _id: organizationId
+    })
+
+    const response = organization.members.filter( (memberId) => memberId.toString() === userId.toString())
+
+    if(!organization || (response.length === 0 && organization.admin.toString() != userId.toString())){
+        res.status(403).json({
+            message: "Either the org doesn't exists or you are not a member of the org"
+        })
+
+        return;
+    }
+
+    const issues = board.issues;
+    const issueArray = await issuesModel.find({
+        _id: issues
+    })
+
+    res.status(200).json({
+        issues: issueArray.map( (iss) => {
+            return {
+                title: iss.title,
+                description: iss.description
+            }
+        })
+    })
 })
 
 app.get("/members", async (req, res) => {
@@ -193,12 +331,18 @@ app.get("/members", async (req, res) => {
         return;
     }
 
-    console.log(organization[0].members)
-    console.log(organization)
-
+    let org = organization[0];
+    const members = await userModel.find({
+        _id: org.members
+    })
 
     res.status(200).json({
-        members: await organization[0].members
+        members: members.map( (m) => {
+            return {
+                username: m.username,
+                id: m._id
+            }
+        })
     })
 
 })
--- Projects/Trello-board/models.js
+++ Projects/Trello-board/models.js
@@ -16,10 +16,26 @@ const organizationSchema = mongoose.Schema({
     members: [mongoose.Types.ObjectId]
 })
 
+const boardSchema = mongoose.Schema({
+    title: String,
+    organization: mongoose.Types.ObjectId,
+    issues: [mongoose.Types.ObjectId]
+})
+
+const issueSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    board: mongoose.Types.ObjectId
+})
+
 userModel = mongoose.model("users", userSchema);
 organizationModel = mongoose.model("organizations", organizationSchema);
+boardModel = mongoose.model("board", boardSchema);
+issuesModel = mongoose.model("issues", issueSchema);
 
 module.exports = {
     userModel,
-    organizationModel
+    organizationModel,
+    boardModel,
+    issuesModel
 }
