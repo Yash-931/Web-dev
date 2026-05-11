f5ecf911412841146e24ef9c990f5e3ba7ae9f59
Yash Aggarwal <yaggarwal931@gmail.com>
2026-05-11 06:22:55 +0530

    Added zod

--- Projects/todo-sql/index.js
+++ Projects/todo-sql/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Pool } = require("pg");
+const z = require("zod");
 require("dotenv").config()
 
 const pool = new Pool({
@@ -9,7 +10,26 @@ const pool = new Pool({
 const app = express();
 app.use(express.json())
 
+
+const SignupSchema = z.object({
+    username: z.string().min(3),
+    password: z.string().min(6),
+    emal: z.email()
+})
+
+
 app.post("/signup", async (req, res) => {
+
+    const { data, success, error } = SignupSchema.safeParse(req.body);
+
+    if(!success){
+        res.status(403).json({
+            message: "Incorrect inputs",
+            error: JSON.parse(error)
+        })
+        return;
+    }
+
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
@@ -22,7 +42,7 @@ app.post("/signup", async (req, res) => {
     await pool.query('INSERT INTO users (username, emal, password) VALUES ($1, $2, $3)', 
         [username, email, password]
     )
-    
+
     res.json({
         message: "Signup successful"
     })
