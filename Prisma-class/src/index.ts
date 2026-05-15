fc6c4d7b7b7ad6d8194f920db37f6007a292cf9f
Yash Aggarwal <yaggarwal931@gmail.com>
2026-05-15 18:31:31 +0530

    fixing prisma-1

--- Prisma-class/src/index.ts
+++ Prisma-class/src/index.ts
@@ -1,3 +1,15 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 
-const client = new PrismaClient();
+const client = new PrismaClient({} as any);
+
+async function createUser() {
+    await client.user.create({
+        data: {
+            username: "Yash",
+            password: "1234",
+            age: 21
+        }
+    })
+}
+
+createUser();
