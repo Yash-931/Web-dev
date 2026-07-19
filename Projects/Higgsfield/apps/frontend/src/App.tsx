5db9f7205e2bea5a944d0da4060259fe55a3db13
Yash-931 <yaggarwal931@gmail.com>
2026-07-19 13:51:48 +0530

    Higgsy frontend init

---
+++ Projects/Higgsfield/apps/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { APITester } from "./APITester";
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="bg-black text-white">
+      Hi there
+    </div>
+  );
+}
+
+export default App;
